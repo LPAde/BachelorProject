20b27b4f5fbe18ff143b9bc76bb0d19b54eac4e2
LPAde <andre-peters01@hotmail.de>
2022-12-02 22:43:56 +0100

    fixed parallx

--- BachelorProject/Assets/Scripts/Jump and Run/UX/ParallaxEffect.cs
+++ BachelorProject/Assets/Scripts/Jump and Run/UX/ParallaxEffect.cs
@@ -6,7 +6,6 @@ namespace Jump_and_Run.UX
     {
         [SerializeField] private float length;
         [SerializeField] private float parallaxValue;
-        [SerializeField] private float minY;
         [SerializeField] private Vector3 startPos;
         [SerializeField] private GameObject cam;
 
@@ -15,7 +14,6 @@ namespace Jump_and_Run.UX
             startPos = transform.position;
             length = GetComponent<SpriteRenderer>().bounds.size.x;
             cam = Camera.main.gameObject;
-            minY = cam.transform.position.y;
         }
 
         private void LateUpdate()
@@ -23,20 +21,11 @@ namespace Jump_and_Run.UX
             var position = cam.transform.position;
             float temp = position.x * (1- parallaxValue);
             float dist = position.x * parallaxValue;
-            
-            float tempY = startPos.y;
-            
-            if (cam.transform.position.y < minY)
-            {
-                startPos.y -= cam.transform.position.y;
-            }
 
             transform.position = new Vector3(startPos.x + dist, startPos.y, startPos.z);
 
             if (temp > startPos.x + length) startPos.x += length;
             else if (temp < startPos.x - length) startPos.x -= length;
-
-            startPos.y = tempY;
         }
     }
 }
